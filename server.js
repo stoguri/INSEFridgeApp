7807acb48c0aaf4b1fba6cdf091f31bee640a163
Nikolas Markides <nikolasmarkides@live.com>
2019-03-01 22:36:17 +0000

    fix github issues, web app adjustments
    improved web app design, install mysql.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require('express');
+const mysql = require ('mysql');
 const app = express();
 const PORT = 8080;
+
 
 
 app.use(express.static(__dirname + '/'));
@@ -15,5 +17,5 @@ app.get('/', function (req, res) {
 //start the server
 app.listen(8080, (err) =>{
     if(err) console.log('error starting the server', err);
-    else console.log('Hello');
+    else console.log('dashboard started on port 8080');
 })
