cffbde4d99047cd85d6ec24157347bb9c83fc821
stoguri <up781439@myport.ac.uk>
2019-04-01 13:54:45 +0100

    update client side JS on index and create function on server.js to handle /index path

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict'
+
+document.getElementById('DropdownButton').addEventListener('click', showDropDown);
+document.getElementById('Recent').addEventListener('click', sortRecent);
+document .getElementById('Alphabetical').addEventListener('click', sortAlphabetical);
+document.getElementById('Quantity').addEventListener('click', sortQuantity);
+
+
+/*toggle between hiding and showing the dropdown content */
+function showDropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  async function sortQuantity() {
+    try {
+      let url = '/index?sort=quantity'
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      putListInPage(await response.json());
+    } catch (e) {
+      console.error('error getting contents of fridge', e);
+
+    }
+  }
+
+  async function sortRecent() {
+    try {
+      let url = '/index?sort=mostRecent';
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      putListInPage(await response.json());
+    } catch(e) {
+      console.error('error getting contents of fridge', e);
+    }
+
+  }
+
+  async function sortAlphabetical() {
+    try {
+      let url = '/index?sort=alphabetical';
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      putListInPage(await response.json());
+    } catch(e) {
+      console.error('error getting contents of fridge', e);
+    }
+
+  }
+
+  function putListInPage(sortedList) {
+    let old = document.getElementsByClassName('row');
+    old.remove();
+    for (i in sortedList) {
+      let newRow = document.createElement("tr");
+      newRow.classList.add("row")
+      let counter = 1
+      for (let j = 0; j < 3; j++) {
+        if (counter = 1) {
+          let newCell = document.createElement('td');
+          let cellText = document.createTextNode(sortedList.name);
+        }
+        else if (counter = 2) {
+          let newCell = document.createElement('td');
+          let cellText = document.createTextNode(sortedList.quantity);
+        } else {
+          let newCell = document.createElement('td');
+          let cellText = document.createTextNode(sortedList.expiry);
+        }
+      }
+      foodTable.appendChild(newRow);
+    }
+  }
+
+}
--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const mysql = require ('mysql');
 const app = express();
 const PORT = 8080;
 
+let foodInFridge = [
+  {
+    name: "apple",
+    quantity: 5,
+    expiry: "05/05/19"
+  },
+]
+
 
 
 app.use(express.static(__dirname + '/'));
@@ -13,6 +21,25 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + 'hello');
 });
+
+// handles requests made on the /index path.
+//Sorts the items in the array of food objects, foodInFridge.
+app.get('/index', function (req, res) {
+    let sortType = req.query.sort;
+    if (sortType = "mostRecent") {
+      res.send(foodInFridge);
+    }
+    else if (sortType = "Alphabetical") {
+      foodInFridge.sort(function(a,b) {return a.name - b.name});
+      res.send(foodInFridge);
+    } else {
+      foodInFridge.sort(function(a,b) {return a.expiry - b.expiry});
+      res.send(foodInFridge);
+    }
+});
+
+
+app.get()
 
 //start the server
 app.listen(8080, (err) =>{
