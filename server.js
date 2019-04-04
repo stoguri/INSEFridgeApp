8e89098c3d2061ab79fe981314ff1dd45887545b
LukeINSE <49232088+LukeINSE@users.noreply.github.com>
2019-04-04 20:04:00 +0100

    Fixed sorting and added sorting for recent

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const DEBUG = true;
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -14,9 +15,12 @@ app.use(express.static('public'));
 app.use('/', (req, res, next) => { console.log(new Date(), req.method, req.url); next(); });
 
 //array that will store food objects
-let foodInFridge = [
-
-]
+let foodInFridge = []
+if (DEBUG) {
+  foodInFridge.push({name: "a", quantity: "3", expiry: "2019-04-02"});
+  foodInFridge.push({name: "b", quantity: "1", expiry: "2019-04-03"});
+  foodInFridge.push({name: "c", quantity: "2", expiry: "2019-04-01"});
+}
 
 //reminder time variables
 let reminderTime = 2;
@@ -61,20 +65,20 @@ app.listen(8080, (err) => {
 ///Server functions
 
 function sortFood(type) {
-  if (type == 'quantity') {
+
+  // for debugging
+  if (DEBUG) {
     console.log(type);
-    let copyList = foodInFridge;
+  }
+
+  let copyList = foodInFridge;
+  if (type == 'quantity') {    
     copyList.sort(function(a,b) {return b.quantity - a.quantity});
-    console.log(copyList)
-    return copyList;
+  } else if (type == 'alphabetical') {
+    copyList.sort(function(a,b) {return b.name - a.name});
+  } else if (type == 'recent') {
+    copyList.sort(function(a,b) {return b.expiry > a.expiry});
   }
-  else if (type == 'alphabetical') {
-    let copyList = foodInFridge;
-    copyList.sort(function(a,b) {return a.name - b.name});
-    console.log(copyList)
-    return copyList;
-  } else {
-    console.log(foodInFridge)
-   return foodInFridge;
-  }
+  console.log(copyList)
+  return copyList;
 }
